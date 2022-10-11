4d5c35f2e2aac56a3ce1421acdaceebd150bdb53
Joe Niksa <jniksa@intrepidcs.com>
2022-10-11 12:39:53 -0400

    First commit to GitHub.

---
+++ README.md
@@ -0,0 +1,10 @@
+# A Place Where I Test New Things
+
+### Test code for practicing.
+
+## Getting Started
+
+### Joe's program
+
+1. Some text
+2. Some other text.
---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main()
+{
+    int file_size; //Snake Case
+    int FileSize; //Pascal Case
+    int fileSize; //Camel Case
+    int iFileSize; //Hungarian Notation (not relevant anymore since we can preview variable types)
+
+    std::cout << "Hello, World!" << std::endl; 
+    return 0;
+}
+
+/*
+ * Comment Block
+ * 
+ * 
+*/
